ac472889bba227e5e24e5fffc0b98e55793a8661
Dylan Tindall <dylantindall2004@gmail.com>
2025-08-19 19:41:28 +0100

    Add new Python files

---
+++ Main.py
@@ -0,0 +1,1 @@
+"This will be responsible for handling user input and displaying the output. "
---
+++ engine.py
@@ -0,0 +1,22 @@
+"This will be responsible for handling the logic of the game."
+
+import numpy as np
+
+class GameState():
+    def __init__(self):
+        self.board = np.array([
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ])
+        self.white_to_move = True
+        self.move_log = []
+
+    def move_log(self):
+
+
